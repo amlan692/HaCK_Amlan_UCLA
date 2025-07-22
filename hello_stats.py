bf5dded2fa92a0f1b6fbf0d955d6fe4ce225047d
amlan692 <amlan.k.mahapatra@gmail.com>
2025-07-22 11:27:51 -0700

    added median function

---
+++ hello_stats.py
@@ -0,0 +1,10 @@
+def median(numbers):
+    numbers.sort()
+    n = len(numbers)
+    mid = n // 2
+    if n % 2 == 0:
+        return (numbers[mid - 1] + numbers[mid]) / 2
+    else:
+        return numbers[mid]
+
+print("hello_world")
